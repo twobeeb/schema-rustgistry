22441ea6dd432ac70ecb30fa5f11c0bc2b136563
Julien Chanaud <chanaud.julien@gmail.com>
2022-09-01 00:55:53 +0200

    Move to axum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use actix_web::error::ErrorNotFound;
-use actix_web::web::Json;
-use actix_web::{get, post, web, App, HttpServer, Responder, Result};
+use axum::{
+    error_handling::HandleErrorLayer,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::sync::Mutex;
+use std::borrow::Cow;
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Subject {
@@ -14,78 +27,82 @@ struct Subject {
 }
 
 struct AppState {
-    schemas_by_name: Mutex<HashMap<String, Subject>>,
-    schemas_by_id: Mutex<HashMap<i32, Subject>>,
-    next_schema_id: Mutex<i32>,
+    schemas_by_name: HashMap<String, Subject>,
+    schemas_by_id: HashMap<i32, Subject>,
+}
+type SharedState = Arc<RwLock<AppState>>;
+
+async fn get_subject_by_name(
+    Path(name): Path<String>,
+    State(data): State<SharedState>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let s = data
+        .read()
+        .unwrap()
+        .schemas_by_name
+        .get(name.as_str())
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+    Ok(Json(s.clone()))
 }
 
-#[get("/subjects/{name}")]
-async fn get_subject_by_name(
-    name: web::Path<String>,
-    data: web::Data<AppState>,
-) -> Result<impl Responder> {
-    match data
-        .schemas_by_name
-        .lock()
-        .unwrap()
-        .get(name.into_inner().as_str())
-    {
-        Some(s) => Ok(Json(s.clone())),
-        _ => Err(ErrorNotFound("Subject not Found")),
-    }
-}
-#[get("/schemas/{id}")]
 async fn get_subject_by_id(
-    id: web::Path<i32>,
-    data: web::Data<AppState>,
-) -> Result<impl Responder> {
-    match data.schemas_by_id.lock().unwrap().get(&(id.into_inner())) {
-        Some(s) => Ok(Json(s.clone())),
-        _ => Err(ErrorNotFound("Subject not Found")),
-    }
-}
-#[get("/subjects")]
-async fn list_subjects(data: web::Data<AppState>) -> impl Responder {
-    let subjects: Vec<Subject> = data
-        .schemas_by_name
-        .lock()
+    Path(id): Path<i32>,
+    State(data): State<SharedState>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let result = data
+        .read()
         .unwrap()
+        .schemas_by_id
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+    Ok(Json(result))
+}
+
+async fn list_subjects(State(data): State<SharedState>) -> impl IntoResponse {
+    let subjects: Vec<Subject> = data
+        .read()
+        .unwrap()
+        .schemas_by_name
         .values()
         .cloned()
         .collect();
-    format!("{subjects:#?}")
+    (StatusCode::OK, Json(subjects))
 }
 
-#[post("/subjects/{name}")]
 async fn register_subject_version(
-    name: web::Path<String>,
-    schema: String,
-    data: web::Data<AppState>,
-) -> impl Responder {
-    let name = name.into_inner().clone();
+    Path(name): Path<String>,
+    //axum::extract::RawBody(schema): String,
+    State(data): State<SharedState>,
+) -> impl IntoResponse {
+    let name = name.to_string();
+    let mut state = data.write().unwrap();
 
-    let mut schemas_by_name = data.schemas_by_name.lock().unwrap();
-    let mut schemas_by_id = data.schemas_by_id.lock().unwrap();
-    let mut next = data.next_schema_id.lock().unwrap();
+    let schema = "empty".to_string();
+
+    let next = state.schemas_by_id.keys().max().unwrap() + 1;
     let subject = Subject {
-        id: *next,
+        id: next,
         name: name.clone(),
         version: 1,
         schema: schema.clone(),
     };
-    schemas_by_name.insert(name.clone(), subject.clone());
-    schemas_by_id.insert(*next, subject.clone());
+    state.schemas_by_name.insert(name.clone(), subject.clone());
+    state.schemas_by_id.insert(next, subject.clone());
 
-    *next += 1;
-
-    format!("New subejct registered: {name}!")
+    (StatusCode::CREATED, Json("New subejct registered: {name}!"))
 }
 
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "example_key_value_store=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     // We suppose that the map is init using external data.
     let mut schemas_by_id: HashMap<i32, Subject> = HashMap::new();
@@ -104,20 +121,52 @@ async fn main() -> std::io::Result<()> {
         .map(|(_k, v)| (v.name.clone(), v.clone()))
         .collect();
 
-    let app_state = web::Data::new(AppState {
-        schemas_by_id: Mutex::new(schemas_by_id.clone()),
-        schemas_by_name: Mutex::new(schemas_by_name.clone()),
-        next_schema_id: Mutex::new(2),
-    });
-    HttpServer::new(move || {
-        App::new()
-            .app_data(app_state.clone())
-            .service(list_subjects)
-            .service(get_subject_by_name)
-            .service(register_subject_version)
-            .service(get_subject_by_id)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let app_state = AppState {
+        schemas_by_id: schemas_by_id.clone(),
+        schemas_by_name: schemas_by_name.clone(),
+    };
+    let shared = SharedState::new(RwLock::new(app_state));
+    let app = Router::with_state(Arc::clone(&shared))
+        .route(
+            "/subjects/:name",
+            get(get_subject_by_name).post(register_subject_version),
+        )
+        .route("/schemas/:id", get(get_subject_by_id))
+        .route("/subjects", get(list_subjects))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                // Handle errors from middleware
+                .layer(HandleErrorLayer::new(handle_error))
+                .load_shed()
+                .concurrency_limit(1024)
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
+
+    // Run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {}", error)),
+    )
 }
